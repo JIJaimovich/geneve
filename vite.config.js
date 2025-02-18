7d82431d0d0c86f54ad3964698a4eb5efc767e50
juani <jaimovich.ignacio@gmail.com>
2025-02-18 13:55:35 -0300

    Config changes

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,12 @@ import autoprefixer from 'autoprefixer';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // base: '/geneve/',
+  base: process.env.NODE_ENV === 'production' ? '/geneve/' : '/',
+  define: {
+    '__VUE_OPTIONS_API__': true,
+    '__VUE_PROD_DEVTOOLS__': false
+  },
   css: {
     postcss: {
       plugins: [tailwindcss(), autoprefixer()],
